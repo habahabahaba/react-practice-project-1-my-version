767bb9dcd0bc668dcdc4a9e9c4cd439e0e295ce3
Alex S <habahabahaba@outlook.com>
2023-04-16 01:47:26 +0400

    Imported UUID V4

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React, { useState } from "react";
 import "./App.css";
 import UserForm from "./components/UserForm/UserForm";
 import UserList from "./components/UserList/UserList";
+import { v4 as uuidv4 } from "uuid";
+
+// const userId = uuidv4();
+// console.log(userId);
 
 function App() {
   return (
