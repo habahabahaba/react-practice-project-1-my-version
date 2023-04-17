da437499d16c9678000522087905c218d246095c
Alex S <habahabahaba@outlook.com>
2023-04-17 19:09:57 +0400

    Added (dummy) UserEntries, ErrorMessages

--- src/App.js
+++ src/App.js
@@ -7,6 +7,17 @@ import { v4 as uuidv4 } from "uuid";
 // const userId = uuidv4();
 // console.log(userId);
 
+const DUMMY_USER_ENTRIES = [{ userId: uuidv4(), userName: "Max", userAge: 31 }];
+
+const [userEntries, setUserEntries] = useState(DUMMY_USER_ENTRIES);
+
+const [error, setError] = useState({
+  errorState: false,
+  errorMessage1: "",
+  errorMessage2: "",
+  errorMessage3: "",
+});
+
 function App() {
   return (
     <div className="App">
