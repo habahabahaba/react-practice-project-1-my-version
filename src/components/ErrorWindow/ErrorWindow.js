d3550cfebe27660db77615004776b0a13864d577
Alex S <habahabahaba@outlook.com>
2023-04-18 20:13:37 +0400

    Error messages styling

--- src/components/ErrorWindow/ErrorWindow.js
+++ src/components/ErrorWindow/ErrorWindow.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./ErrorWindow.css";
 
 function ErrorWindow(props) {
   function clickHandler(event) {
@@ -7,56 +8,18 @@ function ErrorWindow(props) {
 
   return (
     <div>
-      <div
-        style={{
-          display: "block",
-          // position: "fixed",
-          position: "absolute",
-          top: "50%",
-          left: "50%",
-          transform: "translate(-50%, -50%)",
-          width: "60%",
-          padding: "0 0 2% 0",
-          zIndex: "1101",
-          backgroundColor: "white",
-          borderRadius: "7px",
-          // backdropFilter: "blur(2px)",
-        }}
-      >
-        <h2
-          style={{ background: "purple", color: "white", margin: "0 0 5% 0" }}
-        >
+      <div style={{}} className="error-window">
+        <h2 style={{}} className="error-header">
           Invalid input
         </h2>
-        <p>{props.error.errorMessage}</p>
-        <div style={{ textAlign: "end", padding: "3% 3% 0 0" }}>
-          <button
-            onClick={clickHandler}
-            style={{
-              background: "purple",
-              color: "white",
-              border: "none",
-              borderRadius: "1px",
-            }}
-          >
+        <p className="error-message">{props.error.errorMessage}</p>
+        <div style={{}} className="button-area">
+          <button onClick={clickHandler} style={{}} className="button">
             Okay
           </button>
         </div>
       </div>{" "}
-      <div
-        onClick={clickHandler}
-        style={{
-          backdropFilter: "blur(2px)",
-          backgroundColor: "black",
-          opacity: "50%",
-          position: "absolute",
-          top: "0",
-          left: "0",
-          width: "100%",
-          height: "100%",
-          zIndex: "100",
-        }}
-      />
+      <div onClick={clickHandler} style={[]} className="error-overlay" />
     </div>
   );
 }
